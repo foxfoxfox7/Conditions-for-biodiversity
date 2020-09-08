b9ca43a3f2cda49a690794787a0c30628698ff7a
Kieran Fox <kieranfox7@gmail.com>
2020-09-08 22:52:47 +0200

    started on +/-ve indicators

--- site_analysis.py
+++ site_analysis.py
@@ -35,6 +35,7 @@ print('\n\n', site_files, '\n\n')
 site_files = site_files[::-1]
 
 data = []
+spec_data = []
 for site in site_files:
     xls = pd.ExcelFile(site)
 
@@ -59,156 +60,194 @@ for site in site_files:
     year = whole['year'][0].astype(str)
 
     df = pd.DataFrame()
-    print(whole.head())
-    print(ground.head())
 
     ####################################################################
     # extracting the useful information
     ####################################################################
 
-    df['year'] = whole['year']
-    df['bap_b'] = whole['bap_broad']
-    df['bap_p'] = whole['bap_priority']
-    df['light'] = whole['light']
-    df['wetness'] = whole['wetness']
-    df['ph'] = whole['ph']
-    df['fertility'] = whole['fertility']
-    df['competitors'] = whole['competitors']
-    df['stress'] = whole['stress']
-    df['rudereals'] = whole['rudereals']
-    df['nvc'] = whole['nvc_first']
     df['freq_count'] = frequency.gt(0).sum(axis=1)
-    df['max_height'] = ground['max_height']
-    df['median_height'] = ground['median_height']
-    df['freq-bare soil'] = ground['freq-bare soil']
-    df['freq-litter'] = ground['freq-litter']
+
+    extract_cols_w = ['year', 'bap_broad', 'bap_priority', 'light', 'wetness',
+                      'ph', 'fertility', 'competitors', 'stress', 'rudereals',
+                      'nvc_first']
+    for col in extract_cols_w:
+        try:
+            df[col] = whole[col]
+        except:
+            df[col] = np.NaN
+
+    extract_cols_g = ['max_height', 'median_height', 'freq-bare soil',
+                      'freq-litter']
+    for col in extract_cols_g:
+        try:
+            df[col] = ground[col]
+        except:
+            df[col] = np.NaN
+
+    spec_data.append(cover)
     data.append(df)
 
 n_sites = len(data)
+df_t = pd.concat(data)
+col_list = df_t.columns.tolist()
+
+df_spec = pd.concat(spec_data)
+df_spec.columns = df_spec.columns.str.lower()
+spec_l = df_spec.columns.tolist()
 
 ########################################################################
-# plotting the difference between habitats in box and whisker
+# cleaning bap broad
 ########################################################################
 '''
-fig, axes = plt.subplots(nrows=n_sites)
-fig.suptitle('species count')
-for i, ax in zip(range(10), axes.flat):
-    title = data2[i]['year'][0]
-    sns.boxplot(data=data2[i], x='freq_count', y='bap_b', ax=ax).set_title(title)
-plt.show()
+# getting the columns whihc are of object type (to format the strings)
+obj_list = [col for col in col_list if df_t[col].dtype == object]
+print('\nlist of object columns\n', obj_list)
 
-fig, axes = plt.subplots(nrows=n_sites)
-fig.suptitle('species count by bap bap_priority')
-for i, ax in zip(range(10), axes.flat):
-    title = data2[i]['year'][0]
-    sns.boxplot(data=data2[i], x='freq_count', y='bap_p', ax=ax).set_title(title)
-plt.show()
+# turning all strings to lower case due to discrepencies in input
+for col in obj_list:
+    df_t[col] = df_t[col].str.lower()
 
-fig, axes = plt.subplots(nrows=n_sites)
-fig.suptitle('sward heigh (max)')
-for i, ax in zip(range(10), axes.flat):
-    title = data2[i]['year'][0]
-    sns.boxplot(data=data2[i], x='max_height', y='bap_b', ax=ax).set_title(title)
-plt.show()
+# looking at all the unique entries in bap_broad
+baps = df_t['bap_broad'].unique()
+baps = [x for x in baps if str(x) != 'nan']
+print(len(baps), ' unique bap_broad habitats\n', sorted(baps))
+# checking if any of them have names that are similat in case of typo
+clean.check_names(baps, min_ratio = 80)
 
-fig, axes = plt.subplots(nrows=n_sites)
-fig.suptitle('sward heigh (median)')
-for i, ax in zip(range(10), axes.flat):
-    title = data2[i]['year'][0]
-    sns.boxplot(data=data2[i], x='median_height', y='bap_b', ax=ax).set_title(title)
-plt.show()
+# from the dictionary, looking at the proper names for bap_broad
+print(len(lookup['bap_broad']), ' lookup bap_broad\n', sorted(lookup['bap_broad']))
 
- Ask vic if combingin the BARE catagories would be good. so one for each section
-fig, axes = plt.subplots(nrows=n_sites)
-fig.suptitle('frequency of bare SOIL')
-for i, ax in zip(range(10), axes.flat):
-    title = data2[i]['year'][0]
-    sns.boxplot(data=data2[i], x='freq-bare soil', y='bap_b', ax=ax).set_title(title)
-plt.show()
+# These are the names which have typos which need to be replaced
+clean.replace_matches_in_column(df_t, 'bap_broad', 'supralittoral sediment')
+clean.replace_matches_in_column(df_t, 'bap_broad', 'fen, marsh and swamp', 80)
+clean.replace_matches_in_column(df_t, 'bap_broad', 'dwarf shrub heath')
+clean.replace_matches_in_column(df_t, 'bap_broad', 'bogs', 85)
+
+# looking at the names afterward to double check for any more typos
+baps = df_t['bap_broad'].unique()
+baps = [x for x in baps if str(x) != 'nan']
+print(len(baps), ' unique bap_broad habitats\n', sorted(baps))
+
+# looking at how many times each of the unique bap_broad habitats occur
+# many are not in the list so i think they are mistakenly assigned
+# hopefully this means there are not many incidents of them
+bap_types = df_t['bap_broad'].value_counts()
+print(bap_types)
+bap_types = bap_types[bap_types >=9]
+bap_t = bap_types.index.tolist()
+
+# leaving only the parst of the df with the important habitats
+print('\nimportant habitats\n', bap_t)
+df_bap = df_t[df_t['bap_broad'].isin(bap_t)]
+
+# number of bap habitats of interest
+bap_n = len(bap_t)
 '''
 ########################################################################
-# concatting the data and fixing bap_b
+# cleaning nvc
 ########################################################################
+'''
+# removing the inofrmation on how good the nvc fit is
+df_t['nvc_first'] = df_t['nvc_first'].str.partition(':')[0]
+df_t['nvc_first'] = df_t['nvc_first'].str.partition('-')[0]
 
-total_data = pd.concat(data)
+# the subdivisions are too specific to use. not enough samples
+nums = ['0','1','2','3','4','5','6','7','8','9']
+for nn in nums:
+    df_t['nvc_first'] = df_t['nvc_first'].str.partition(nn)[0]
 
-total_data = total_data[total_data['bap_b'].notna()]
-total_data['bap_b'] = total_data['bap_b'].str.lower()
+df_t['nvc_first'] = df_t['nvc_first'].replace({
+    "w": 'woodlands and scrub',
+    'm': 'mires',
+    "h": 'heathes',
+    'mg': 'mesotrophic grasslands',
+    'cg': 'calcicolous grasslands',
+    'u': 'calcifugous grasslands',
+    'a': 'aquatic communities',
+    's': 'swamps and tall herb ferns',
+    'sd': 'shingle, sandline and sand-dune',
+    'sm': 'salt marsh',
+    'mc': 'maritime cliff',
+    'ov': 'vegetation of open habitats'
+    })
 
-names = total_data['bap_b'].unique()
-print('\nnames\n')
-for nn in names:
-    print(nn)
-names = [x for x in names if str(x) != 'nan']
-clean.check_names(names, min_ratio = 80)
+# counting how many of each NVC types has been assigned
+# only taking the types that have enough samples
+nvc_types = df_t['nvc_first'].value_counts()
+print(nvc_types)
+nvc_types = nvc_types[nvc_types >=9]
+nvc_t = nvc_types.index.tolist()
 
-# Saltfleetby
-#clean.replace_matches_in_column(total_data, 'bap_b', 'fen, marsh and swamp', min_ratio = 80)
-#clean.replace_matches_in_column(total_data, 'bap_b', 'supralittoral sediment')
+# leaving only the parts of the df with the important habitats
+print('\nimportant nvc types\n', nvc_t)
+df_nvc = df_t[df_t['nvc_first'].isin(nvc_t)]
 
-bap_b = total_data['bap_b'].unique()
-print(bap_b)
-fig_num = len(bap_b)
+# plotting the values counts of the habitats per year as some years
+# may not have neough to draw valuable conclusions
+years = df_nvc['year'].unique()
+for yy in years:
+    df_t = df_nvc[df_nvc['year'] == yy]
+    print('\n', yy, '\n')
+    print(df_t['nvc_first'].value_counts())
 
+# number of nvc habitats of interest
+nvc_n = len(nvc_t)
+'''
 ########################################################################
 # plotting the difference between years in box and whisker
 ########################################################################
-
+'''
 def plot_by_bap_vs_year(y_col, title = '', save = False, show = True):
 
-    fig, axes = plt.subplots(ncols=fig_num, sharey=True, dpi=250)
+    fig, axes = plt.subplots(ncols=bap_n, sharey=True, dpi=250)
     fig.suptitle(site_name + ' - ' + title)
     for i, ax in zip(range(10), axes.flat):
-        sns.boxplot(data = total_data[total_data['bap_b'] == bap_b[i]],
-            x='year', y=y_col, ax=ax).set_title(bap_b[i], fontsize=7)
+        sns.boxplot(data = df_bap[df_bap['bap_broad'] == bap_t[i]],
+            x='year', y=y_col, ax=ax).set_title(bap_t[i], fontsize=10)
+
+    # have to go through each axes individually to set the angle
+    for ax in fig.axes:
+        plt.sca(ax)
+        plt.xticks(rotation=45)
+
+    # turns the y axis off for all plots apart from first to save space
+    for nn in range(bap_n-1):
+        axes[nn+1].get_yaxis().set_visible(False)
+
     if save:
         plt.savefig('./figures/' + site_name + '_' + title + '.png')
     if show:
         plt.show()
 
-#plot_by_bap_vs_year('freq_count', 'species_count')
-#plot_by_bap_vs_year('max_height', 'sward_max')
-#plot_by_bap_vs_year('median_height', 'sward med')
-#plot_by_bap_vs_year('freq-bare soil', 'bare_soil_freq')
-#plot_by_bap_vs_year('light', 'light')
-#plot_by_bap_vs_year('wetness', 'wetness')
-#plot_by_bap_vs_year('ph', 'ph')
-#plot_by_bap_vs_year('fertility', 'fertility')
-#plot_by_bap_vs_year('competitors', 'competitors')
-#plot_by_bap_vs_year('stress', 'stress')
-#plot_by_bap_vs_year('rudereals', 'rudereals')
-
-########################################################################
-# nvc
-########################################################################
-
-
-
-total_data['nvc'] = total_data['nvc'].str.partition(':')[0]
-total_data['nvc'] = total_data['nvc'].str.replace('[a-z]', '')
-total_data['nvc_edit'] = total_data['nvc'].str.replace('[0-9]', '')
-
-#print(total_data['nvc'].value_counts())
-
-years = total_data['year'].unique()
-for yy in years:
-    df = total_data[total_data['year'] == yy]
-    #print('\n', yy, '\n')
-    #print(df['nvc_edit'].value_counts())
-
-nvc_types = total_data['nvc_edit'].value_counts()
-nvc_types = nvc_types[nvc_types >=10]
-nvc_t = nvc_types.index.tolist()
-
-print(nvc_t)
+plot_by_bap_vs_year('freq_count', 'species_count')
+plot_by_bap_vs_year('max_height', 'sward_max')
+plot_by_bap_vs_year('median_height', 'sward med')
+plot_by_bap_vs_year('freq-bare soil', 'bare_soil_freq')
+plot_by_bap_vs_year('light', 'light')
+plot_by_bap_vs_year('wetness', 'wetness')
+plot_by_bap_vs_year('ph', 'ph')
+plot_by_bap_vs_year('fertility', 'fertility')
+plot_by_bap_vs_year('competitors', 'competitors')
+plot_by_bap_vs_year('stress', 'stress')
+plot_by_bap_vs_year('rudereals', 'rudereals')
 
 def plot_by_nvc_vs_year(y_col, title = '', save = False, show = True):
 
-    fig, axes = plt.subplots(ncols=fig_num, sharey=True, dpi=250)
+    fig, axes = plt.subplots(ncols=nvc_n, sharey=True, dpi=250)#
     fig.suptitle(site_name + ' - ' + title)
     for i, ax in zip(range(10), axes.flat):
-        sns.boxplot(data = total_data[total_data['nvc_edit'] == nvc_t[i]],
-            x='year', y=y_col, ax=ax).set_title(nvc_t[i], fontsize=12)
+        sns.boxplot(data = df_nvc[df_nvc['nvc_first'] == nvc_t[i]],
+            x='year', y=y_col, ax=ax).set_title(nvc_t[i], fontsize=6)
+
+    # have to go through each axes individually to set the angle
+    for ax in fig.axes:
+        plt.sca(ax)
+        plt.xticks(rotation=45)
+
+    # turns the y axis off for all plots apart from first to save space
+    for nn in range(nvc_n-1):
+        axes[nn+1].get_yaxis().set_visible(False)
+
     if save:
         plt.savefig('./figures/' + site_name + '_' + title + '.png')
     if show:
@@ -216,40 +255,81 @@ def plot_by_nvc_vs_year(y_col, title = '', save = False, show = True):
 
 
 plot_by_nvc_vs_year('freq_count', 'species_count')
-#plot_by_nvc_vs_year('max_height', 'sward_max')
-#plot_by_nvc_vs_year('median_height', 'sward med')
-#plot_by_nvc_vs_year('freq-bare soil', 'bare_soil_freq')
-#plot_by_nvc_vs_year('light', 'light')
-#plot_by_nvc_vs_year('wetness', 'wetness')
-#plot_by_nvc_vs_year('ph', 'ph')
-#plot_by_nvc_vs_year('fertility', 'fertility')
-#plot_by_nvc_vs_year('competitors', 'competitors')
-#plot_by_nvc_vs_year('stress', 'stress')
-#plot_by_nvc_vs_year('rudereals', 'rudereals')
-#plot_by_nvc_vs_year('freq-litter', 'freq-litter')
-
+plot_by_nvc_vs_year('max_height', 'sward_max')
+plot_by_nvc_vs_year('median_height', 'sward med')
+plot_by_nvc_vs_year('freq-bare soil', 'bare_soil_freq')
+plot_by_nvc_vs_year('light', 'light')
+plot_by_nvc_vs_year('wetness', 'wetness')
+plot_by_nvc_vs_year('ph', 'ph')
+plot_by_nvc_vs_year('fertility', 'fertility')
+plot_by_nvc_vs_year('competitors', 'competitors')
+plot_by_nvc_vs_year('stress', 'stress')
+plot_by_nvc_vs_year('rudereals', 'rudereals')
+plot_by_nvc_vs_year('freq-litter', 'freq-litter')
+'''
 ########################################################################
-# correlation
+# looking for correlations across different habitats
 ########################################################################
+'''
+# finding the columns with numerical entries to compare
+float_list = [col for col in col_list if df[col].dtype == float]
+print('\nlist of float columns\n', float_list)
 
-#g = sns.PairGrid(total_data, hue="year")
-#g.map_diag(plt.hist)
-#g.map_offdiag(plt.scatter)
-#g.add_legend()
-#plt.show()
+# dropping a few columns that don't have any meaningful information
+drop_l = ['freq-litter', 'median_height', 'max_height', 'year']
+df_bap = df_bap.drop(drop_l, axis=1)
+float_list = [val for val in float_list if val not in drop_l]
+print(float_list)
 
-col_list = total_data.columns.tolist()
-col_list2 = [col for col in col_list if total_data[col].dtype == float]
-
-#for var in col_list2:
-#    sns.relplot(data=total_data, x='freq_count', y=var, hue='bap_b')
-#    plt.show()
-
-nvc_df = total_data[total_data['nvc_edit'].isin(nvc_t)]
-drop_l = ['freq-litter', 'median_height', 'max_height']
-nvc_df = nvc_df.drop(drop_l, axis=1)
-col_list2 = [val for val in col_list2 if val not in drop_l]
-
-for var in col_list2:
-    sns.lmplot(data=nvc_df, x='freq_count', y=var, col='nvc_edit')
+for var in float_list:
+    sns.lmplot(data=df_bap, x='freq_count', y=var, col='bap_broad',
+        sharex=False)
     plt.show()
+
+# finding the columns with numerical entries to compare
+float_list = [col for col in col_list if df[col].dtype == float]
+print('\nlist of float columns\n', float_list)
+
+# dropping a few columns that don't have any meaningful information
+drop_l = ['freq-litter', 'median_height', 'max_height', 'year',
+    'freq-bare soil']
+df_nvc = df_nvc.drop(drop_l, axis=1)
+float_list = [val for val in float_list if val not in drop_l]
+print(float_list)
+
+for var in float_list:
+    sns.lmplot(data=df_nvc, x='freq_count', y=var, col='nvc_first',
+        sharex=False)#.set_title('lalala')
+    plt.show()
+'''
+########################################################################
+# positive/negatie indicator species
+########################################################################
+
+print(spec_l)
+print(len(spec_l))
+
+clean.check_names(spec_l)
+
+df_spec_t = pd.DataFrame(index=spec_l)
+df_spec_t['spec list'] = spec_l
+df_spec_t['bryophytes and lichens'] = None
+df_spec_t['dwarf shrubs'] = None
+df_spec_t['ulex or genista'] = None
+df_spec_t['non-graminae'] = None
+df_spec_t['graminoids'] = None
+df_spec_t['cg3+ positive'] = None
+df_spec_t['cg2 positive'] = None
+df_spec_t['H desirable forbs'] = None
+df_spec_t['H exotic species -ve'] = None
+df_spec_t['H Acrocarpous mosses'] = None
+df_spec_t['bracken'] = None
+df_spec_t['cg3 -ve 5'] = None
+df_spec_t['cg3 -ve 10'] = None
+df_spec_t['cg2 -ve 5'] = None
+df_spec_t['cg2 -ve 10'] = None
+df_spec_t['h -ve 1'] = None
+df_spec_t['cg -ve tree and scrub'] = None
+df_spec_t['h -ve tree and scrub'] = None
+
+print(df_spec_t.head())
